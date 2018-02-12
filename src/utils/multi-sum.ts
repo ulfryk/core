2dd4409c9adde241efac0e4eb0747015c7f92ec4
Jakub Strojewski <jakub.strojewski@gmail.com>
2018-02-12 14:27:18 +0100

    PC-1089: Fix array helpers typings

--- src/utils/multi-sum.ts
+++ src/utils/multi-sum.ts
@@ -1,4 +1,16 @@
-export const multiSum = <T>(items: T[], ...accessors: ((item: T) => number)[]): number[] =>
-  items.reduce((acc: number[], item: T) => accessors
-      .map((accessor, index) => acc[index] + accessor(item)),
-    accessors.map(() => 0));
+export interface IArrayMultiSum {
+  <T>(
+    items: T[],
+    ...accessors: ((item: T, index: number, array: T[]) => number)[],
+  ): number[];
+  <T>(
+    items: ReadonlyArray<T>,
+    ...accessors: ((item: T, index: number, array: ReadonlyArray<T>) => number)[],
+  ): ReadonlyArray<T>;
+}
+
+export const multiSum: IArrayMultiSum =
+  (items: any, ...accessors: any[]) =>
+    items.reduce((acc: any, item: any, i: any, array: any) => accessors
+        .map((accessor, j) => acc[j] + accessor(item, i, array)),
+      accessors.map(() => 0) as any);
