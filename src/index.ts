93795e7f942efbecc1045dfbffb32ded792e5ce4
Tomasz Olędzki <luksfer@poczta.onet.pl>
2018-03-15 16:15:57 +0100

    export services

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import './global-imports'; // tslint:disable-line:no-import-side-effect
 
 export * from './decorators';
 export * from './model';
+export * from './services';
 export * from './utils';
