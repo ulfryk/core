d6b2a996c29cba3add400317387078912f92eb42
Tomasz Olędzki <luksfer@poczta.onet.pl>
2018-03-15 16:17:07 +0100

    Do not export queue in index.ts

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,7 +1,6 @@
 export * from './common';
 export * from './log-level';
 export * from './plainable';
-export * from './queue';
 export * from './request-state';
 export * from './serialized-either';
 export * from './sort-order';
